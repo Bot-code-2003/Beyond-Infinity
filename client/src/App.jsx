74a2dae1b2b7585cdde12e83a26a0f77b56dd900
Dharmadeep <dharmadeepmadisetty@gmail.com>
2024-11-22 15:30:03 +0530

    Vercel ANalytics added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import AfterLogout from "./pages/AfterLogout";
 import EmailComponent from "./components/EmailComponent";
 import Footer from "./components/Footer";
 import { Helmet } from "react-helmet";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   const user = localStorage.getItem("User");
@@ -25,6 +26,7 @@ const App = () => {
   };
   return (
     <div className="bg-gray-50">
+      <Analytics />
       {!user && (
         <div className="hidden">
           <GoogleLogin
